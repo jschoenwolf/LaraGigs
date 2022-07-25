d5c38e8777f07c962d71eda7ae182526586d524e
jschoenwolf <jschoenwolf@gmail.com>
2022-07-24 22:34:52 -0700

    Here we go!!

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => [
+
+            [
+                'id' => 1,
+                'title' => 'Listing One',
+                'description' => 'Now you know, but if the symbols confuse you, don’t feel too bad about it. I’ve been using Macs since I was a little kid and the Option and Control key symbols have always perplexed me to the point where I’ll forget which each is, and that is precisely why Apple is gradually moving to the labeled keys rather than symbol keys. Simple is better'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing Two',
+                'description' => 'Now you know, but if the symbols confuse you, don’t feel too bad about it. I’ve been using Macs since I was a little kid and the Option and Control key symbols have always perplexed me to the point where I’ll forget which each is, and that is precisely why Apple is gradually moving to the labeled keys rather than symbol keys. Simple is better'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Listing Three',
+                'description' => 'Now you know, but if the symbols confuse you, don’t feel too bad about it. I’ve been using Macs since I was a little kid and the Option and Control key symbols have always perplexed me to the point where I’ll forget which each is, and that is precisely why Apple is gradually moving to the labeled keys rather than symbol keys. Simple is better'
+            ]
+        ]
+    ]);
 });
