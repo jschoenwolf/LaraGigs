ee469f43248a1da8fdb613c254fd0cdb050859da
jschoenwolf <jschoenwolf@gmail.com>
2022-07-27 14:45:56 -0700

    more tutorial

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,37 +2,10 @@
 
 namespace App\Models;
 
-class Listing
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
 {
-
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'Now you know, but if the symbols confuse you, don’t feel too bad about it. I’ve been using Macs since I was a little kid and the Option and Control key symbols have always perplexed me to the point where I’ll forget which each is, and that is precisely why Apple is gradually moving to the labeled keys rather than symbol keys. Simple is better'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'Do you know, but if the symbols confuse you, don’t feel too bad about it. I’ve been using Macs since I was a little kid and the Option and Control key symbols have always perplexed me to the point where I’ll forget which each is, and that is precisely why Apple is gradually moving to the labeled keys rather than symbol keys. Simple is better'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Listing Three',
-                'description' => 'If you know, but if the symbols confuse you, don’t feel too bad about it. I’ve been using Macs since I was a little kid and the Option and Control key symbols have always perplexed me to the point where I’ll forget which each is, and that is precisely why Apple is gradually moving to the labeled keys rather than symbol keys. Simple is better'
-            ]
-        ];
-    }
-    public static function find($id)
-    {
-        $listings = self::all();
-
-        foreach ($listings as $listing) {
-            if ($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
+    use HasFactory;
 }
